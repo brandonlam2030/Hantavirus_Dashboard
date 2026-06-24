f993443d202c73be80d2d78ea68da80990944303
brandonlam2030 <brandonlam2030@gmail.com>
2026-06-24 14:47:20 -0700

    adjusted data filtering and report submissions

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ gc = gspread.service_account_from_dict(st.secrets["AUTH"])
 sheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/1OMYd89ViYPjpKFo28gV1dnuWBuQMuIgG5HCTZpReO7U/edit?gid=0#gid=0")
 worksheet = sheet.sheet1
 coords = sheet.worksheet("coords")
+fieldAgentForm = sheet.worksheet("FA")
+hospitalForm = sheet.worksheet("HR")
 
 title, button = st.columns([20,1])
 powerBI = ""
@@ -40,9 +42,10 @@ cases = cases[1:].reset_index(drop = True)
 
 coordinates = pd.DataFrame(coords.get_all_values(), columns = ["country", "lat", "lon", "count"])
 coordinates = coordinates[1:].reset_index(drop = True)
-coordinates = coordinates[coordinates["count"].astype("float32") > 0]
+coordinates["count"] = coordinates["count"].astype(int)
 
-
+fieldAgentDF = pd.DataFrame(fieldAgentForm.get_all_values(), columns = ["patient_id", "year", "country", "syndrome", "age", "sex", "symptoms"])
+hospitalReportDF = pd.DataFrame(hospitalForm.get_all_values(), columns = ["patient_id", "year", "country", "syndrome", "age", "sex", "symptoms"])
 
 with tab1:
     box1, box2, box3 = st.columns(3)
@@ -99,63 +102,82 @@ with tab1:
     if globeClick and globeClick.selection.points:
         st.session_state.clickedPoints = globeClick.selection.points[0]
 
-
-
-
-
-
 with tab4:
-    contact = st.form("Report a case", clear_on_submit = True)
-
     cases["symptoms"] = cases["symptoms"].str.split(",")
     uniqueSymptoms = cases["symptoms"].explode().str.strip().unique()
-    
-    name = contact.text_input("Full name:")
-    city = contact.text_input("Enter city:")
-    state = contact.text_input("Enter state:")
-    country =  contact.text_input("Enter country:")
-    zipCode = contact.text_input("Enter zipCode:")
-    email = contact.text_input("Email:")
-    
-    #need fixing on structure in future
-    age, sex = st.columns(2)
-    with age:
-        ageNum = contact.number_input("Age", min_value = 1)
-    with sex:
-        sexVal = contact.segmented_control("Sex", ["Male", "Female"])
-    
-    symptoms = contact.multiselect("Symptom(s) (Select all that apply)", uniqueSymptoms)
+    def contactForm(id:str):
+        contact = st.form(id, clear_on_submit = True)
+        
+        name = contact.text_input("Full name:")
+        city = contact.text_input("Enter city:")
+        state = contact.text_input("Enter state:")
+        country =  contact.text_input("Enter country:")
+        zipCode = contact.text_input("Enter zipCode:")
+        email = contact.text_input("Email:")
+        
+        #need fixing on structure in future
+        age, sex = contact.columns(2)
+        with age:
+            ageNum = contact.number_input("Age", min_value = 1)
+        with sex:
+            sexVal = contact.segmented_control("Sex", ["Male", "Female"])
+        
+        symptoms = contact.multiselect("Symptom(s) (Select all that apply)", uniqueSymptoms)
+        
+
+        submit = contact.form_submit_button("Submit")
     
 
-    submit = contact.form_submit_button("Submit")
-    
 
-    if submit:
-        if any(len(str(value)) == 0 for value in [name, city, state, country, zipCode, email, ageNum, sexVal, symptoms]): st.warning("Please fill out all sections!!")
-        elif ageNum < 0: st.warning("Please enter a valid age")
-        else:
-            currentId = int(cases["patient_id"].iloc[-1][cases["patient_id"].iloc[-1].find("-")+1:])+1
-            results = [currentId, datetime.datetime.now().year, country, "None", ageNum, sexVal, ", ".join(symptoms)]
-            
-            
-            if country in coordinates["country"]:
-                worksheet.append_row(results)
-                coordinates.iloc[coordinates["country"] == country, "count"] += 1
-                st.success("Form submitted successfully!")
+        if submit:
+            if any(len(str(value)) == 0 for value in [name, city, state, country, zipCode, email, ageNum, sexVal, symptoms]): st.warning("Please fill out all sections!!")
             else:
-                st.warning("Please enter a valid country")
+                currentSheet = None
+                if id == "FA": currentSheet = fieldAgentDF["patient_id"]
+                elif id == "FA": currentSheet = hospitalReportDF["patient_id"]
+
+                if currentSheet.size == 0:
+                    currentId = 0
+                else:
+                    currentId = int(currentSheet.iloc[-1])+1
+                results = [currentId, datetime.datetime.now().year, country, "None", ageNum, sexVal, ", ".join(symptoms)]
+                
+                
+                if country not in coordinates["country"].values:
+                    
+                    st.warning("Please enter a valid country")
+
+                else:
+                    if id == "FA":
+                        fieldAgentForm.append_row(results)
+                    elif id == "HR":
+                        hospitalForm.append_row(results)
+
+                    rowIdx = coordinates.index[coordinates["country"] == country].tolist()[0]
+                    currentCount = coordinates.loc[rowIdx, "count"] + 1
+                    coordinates.loc[rowIdx, "count"] = currentCount 
+                    coords.update_cell(rowIdx + 2, 4, int(currentCount))
+
+                    st.success("Form submitted successfully!")
+
+    fieldAgent = st.expander("Submit field agent data")
+    hospitalReport = st.expander("Report Hospital Cases")
+    socialMedia = st.expander("")
+
+    with fieldAgent:
+        contactForm("FA")
+    
+    with hospitalReport:
+        contactForm("HR")
 
 
 with tab2:
-    dataTable = st.empty()
-    dataTable.empty()
-    
     targetData = cases.copy()
-    for item in st.session_state.filters:
-        targetData = targetData[targetData[item[0]] == item[1]]
-
-    dataTable.table(targetData[["patient_id", "year", "country", "syndrome", "age", "sex", "symptoms", "severity", "geographic_setting", "case_status"]].iloc[st.session_state.pageIdx:st.session_state.pageIdx+15])
     
+
+    if st.session_state.filters:
+        targetData = targetData[targetData[st.session_state.filters[0][0]] == st.session_state.filters[0][1]]
+
 
     left, r1, right = st.columns([1,25,1])
 
@@ -187,14 +209,22 @@ with tab2:
                 specific = st.text_input("SPECIFICS", label_visibility = "collapsed")
             
             with formattedCol[4]:
-                filterSubmit = st.form_submit_button("Filter", use_container_width = True)
+                if st.form_submit_button("Filter", use_container_width = True):
+                    replacementKeys = {
+                        "Patient ID": "patient_id",
+                        "Geographic Setting": "geographic_setting",
+                        "Case Status": "case_status"
+                    }
+                    
+                    selectFilter = replacementKeys.get(selectFilter,selectFilter.lower())
+                    st.session_state.filters.clear()
+                    st.session_state.pageIdx = 0
 
-                if filterSubmit:
-                    st.session_state.filters.append([selectFilter, specific])
-
+                    if selectFilter:
+                        st.session_state.filters.append([selectFilter, specific])
         
 
-    
+    st.table(targetData[["patient_id", "year", "country", "syndrome", "age", "sex", "symptoms", "severity", "geographic_setting", "case_status"]].iloc[st.session_state.pageIdx:st.session_state.pageIdx+15])
 
 
     

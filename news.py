d75972a0df0abf2962a5dbb8ef07f88bf7cd9ae7
brandonlam2030 <brandonlam2030@gmail.com>
2026-06-29 12:50:59 -0700

    added count to function return value

--- news.py
+++ news.py
@@ -2,15 +2,15 @@ from newsapi import NewsApiClient
 import streamlit as st
 
 newsapi = NewsApiClient(api_key = st.secrets["NEWS_API"])
+headlines = ""
 
 def getNews():
     headlines = newsapi.get_everything(
         q = "hantavirus",
-        language = "en",
         sort_by = "relevancy"
     )
-    
     if headlines["status"] == "ok":
-        return headlines["articles"][0:10]
+        return [headlines["articles"][0:10], headlines["totalResults"]]
     
     return []
+

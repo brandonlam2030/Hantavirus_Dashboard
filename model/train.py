b142f90f72998fd20dc7fb3199306fd7268934a2
Brandon Lam <brandonlam2030@gmail.com>
2026-07-07 21:27:58 -0700

    splitting data for xgboost regression

--- model/train.py
+++ model/train.py
@@ -4,10 +4,15 @@ from xgboost import XGBClassifier as xgb
 import pandas as pd
 
 data = pd.read_csv("aggregatedData.csv")
+data = pd.get_dummies(data, columns=['siteID'])
 
 text_columns = data.select_dtypes(include=['object', 'category']).columns
 
 for column in text_columns:
     data[column] = LabelEncoder.fit_transform(data[column])
 
-x_train, x_test, y_train, y_test = train_test_split()
+data = data.sort_values("collectDate")
+split = "2023-01-01"
+train = data[data["collectDate"] < split]
+test = data[data["collectDate"] >= split]
+
